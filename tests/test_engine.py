944101854ef2957038a35f42f83c9d1dabd50fdd
Brendan Horst <yoshistar2000@gmail.com>
2020-03-26 12:38:37 -0400

    Wrote map tests and a few comments in test_engine.py

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -9,9 +9,11 @@ def test_interface(client, app):
     assert b'Enter Command:' in response.data
     assert b'<button type="submit"' in response.data
 
+    #Moving north from the start (middle) should return the north room
     response = client.post('/', data={'command': 'move north'})
     assert b'You are in the northern room' in response.data
     assert 'current_room=north_room' in response.headers['Set-Cookie']
 
+    #Ensures that inputting a false command does not go back to the beginning
     response = client.post('/', data={'command': ' '})
     assert b'Welcome to Dungeon Escape!' not in response.data
