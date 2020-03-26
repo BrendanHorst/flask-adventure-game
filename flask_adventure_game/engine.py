da5eff82d6d9f8f594926deff683d7d863fe1b8e
Brendan Horst <yoshistar2000@gmail.com>
2020-03-26 16:07:11 -0400

    Incorporated the lexicon into engine.py

--- flask_adventure_game/engine.py
+++ flask_adventure_game/engine.py
@@ -3,6 +3,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for, make_response
 )
 from flask_adventure_game.map import Map
+from flask_adventure_game.lexicon import scan
 
 bp = Blueprint('engine', __name__)
 
@@ -12,23 +13,22 @@ def game():
     if (request.method == 'POST'):
 
         map = Map()
-        command = request.form['command']
+        command = scan(request.form['command'])
         location = request.cookies.get('current_room')
         if location == None:
             location = 'middle_room'
 
-        if 'north' in command:
-            destination = getattr(map, location).adjacent_rooms['north']
+        if command['type'] == 'movement':
+
+            destination = getattr(map, location).adjacent_rooms[command['detail']]
+
             if destination:
+
                 resp = make_response(render_template('index.html', message=map.next_room(destination)))
                 resp.set_cookie('current_room', destination)
+
                 return resp
-        elif 'south' in command:
-            destination = getattr(map, location).adjacent_rooms['south']
-            if destination:
-                resp = make_response(render_template('index.html', message=map.next_room(destination)))
-                resp.set_cookie('current_room', destination)
-                return resp
+
 
         resp = make_response(render_template('index.html', message=map.next_room(location)))
         resp.set_cookie('current_room', location)
