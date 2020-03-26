fba5becf21f14c89d4cdf4fbe90c6a5c154b4dc1
Brendan Horst <yoshistar2000@gmail.com>
2020-03-26 12:50:12 -0400

    Wrote more tests more movement

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -14,6 +14,16 @@ def test_interface(client, app):
     assert b'You are in the northern room' in response.data
     assert 'current_room=north_room' in response.headers['Set-Cookie']
 
+    #Moving south from the start (middle) should return the south room
+    response = client.post('/', data={'command': 'move south'})
+    assert b'You are in the southern room' in response.data
+    assert 'current_room=south_room' in response.headers['Set-Cookie']
+
+    #Moving left or right should stay in the current room (middle)
+    response = client.post('/', data={'command': 'move east'})
+    assert b'You are in the middle room' in response.data
+    assert 'current_room=middle_room' in response.headers['Set-Cookie']
+
     #Ensures that inputting a false command does not go back to the beginning
     response = client.post('/', data={'command': ' '})
     assert b'Welcome to Dungeon Escape!' not in response.data
