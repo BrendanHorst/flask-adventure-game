f0ef7167febbfe8462ad5e4d5e7fc1f322a80c82
Brendan Horst <yoshistar2000@gmail.com>
2020-03-26 15:15:41 -0400

    Fixed words object and added check for empty input in scan()

--- flask_adventure_game/lexicon.py
+++ flask_adventure_game/lexicon.py
@@ -1,8 +1,16 @@
 words = {
-    'directions' = ['north', 'south', 'east', 'west']
-    'movement' = ['go', 'move', 'head']
-    'action' = ['jump', 'fly', 'leave', 'open', 'drink', 'sip', 'chug', 'throw', 'toss', 'drop', 'offer']
+    'directions' : ['north', 'south', 'east', 'west'],
+    'movement' : ['go', 'move', 'head'],
+    'action' : ['jump', 'fly', 'leave', 'open', 'drink', 'sip', 'chug', 'throw', 'toss', 'drop', 'offer']
 }
 
 def scan(input):
-    pass
+
+    output = { 'type': 'error', 'detail': 'Unknown error'}
+
+    #Checks for an empty command submission
+    if input == '':
+        output['detail'] = 'Enter a command'
+        return output
+
+    return output
