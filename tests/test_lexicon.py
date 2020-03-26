932b2a14218711ccff161ff8b42e29ad74e6d93a
Brendan Horst <yoshistar2000@gmail.com>
2020-03-26 15:11:49 -0400

    Wrote lexicon tests for movement commands

--- tests/test_lexicon.py
+++ tests/test_lexicon.py
@@ -1,1 +1,26 @@
 from flask_adventure_game.lexicon import scan
+
+
+def test_empty_command():
+
+    command = scan('')
+    assert command['type'] == 'error'
+    assert command['detail'] == 'Enter a command'
+
+def test_movement_commands():
+
+    command = scan('Go north')
+    assert command['type'] == 'direction'
+    assert command['detail'] == 'north'
+
+    command = scan('HEAD WEST')
+    assert command['type'] == 'direction'
+    assert command['detail'] == 'west'
+
+    command = scan('slither south')
+    assert command['type'] == 'error'
+    assert command['detail'] == "I don't know what that means"
+
+    command = scan('move')
+    assert command['type'] == 'error'
+    assert command['detail'] == 'Please give a cardinal direction'
